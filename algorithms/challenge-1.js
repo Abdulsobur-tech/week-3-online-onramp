6e70b17dc4c4ad7ec1bc8659a8fb37bd08f2fd9b
Abdulsobur <abdulsoburtaofeeq@gmail.com>
2022-03-13 07:28:40 -1200

    week-3

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,14 @@
-function romanToDecimal(roman) {}
-
-module.exports = romanToDecimal
+function romanToDecimal(roman) {
+    if(typeof roman !== 'number')
+    return false
+let digits = String (+roman).split(""),
+key =["","C","CC","CCC","CD","CD","D","DC","DCCC","CM","","X","XX","XXX","XL",
+"L","LX","LXX","LXXX","XC","","I","II","III","IV","V","VII","VII","VIII","IX"],
+roman_num ="",
+i =3;
+while(i--)
+roman_num =(key[+digits.pop() + (i * 10)] || "") + roman_num;
+return Array (+digits.join("") +1).join("M") + roman_num;
+}
+ console.log(romanToDecimal(1))
+//module.exports = romanToDecimal
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,9 @@
-function transpose(arr) {}
-
-module.exports = transpose
+function transpose(arr) {
+   return transpose = m => m[0].map((x,i) => m.map(x => x[i]))
+}
+console.log(transpose([
+    [5, 12, 17, 9, 3],
+    [13, 4, 8, 14, 1],
+    [9, 6, 3, 7, 21],
+  ]))
+//module.exports = transpose
